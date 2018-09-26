d5769a698d3007e089df60b1a2c72aab94623496
vujasm <m.vujasinovic@innova-eu.net>
2018-09-26 16:04:04 +0200

    code improvement

--- src/main/java/nimble/trust/engine/collector/ProfileCompletnessCollector.java
+++ src/main/java/nimble/trust/engine/collector/ProfileCompletnessCollector.java
@@ -1,6 +1,7 @@
 package nimble.trust.engine.collector;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,73 +9,95 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.google.common.collect.Lists;
+
+import eu.nimble.service.model.ubl.commonaggregatecomponents.PartyType;
+import eu.nimble.service.model.ubl.commonaggregatecomponents.QualityIndicatorType;
+import eu.nimble.service.model.ubl.commonbasiccomponents.QuantityType;
 import nimble.trust.engine.domain.TrustAttribute;
 import nimble.trust.engine.domain.TrustProfile;
 import nimble.trust.engine.model.vocabulary.Trust;
+import nimble.trust.engine.restclient.CatalogServiceClient;
+import nimble.trust.engine.restclient.IdentityServiceClient;
 import nimble.trust.engine.service.TrustProfileService;
-
 
 @Service
 public class ProfileCompletnessCollector {
 
 	private static Logger log = LoggerFactory.getLogger(ProfileCompletnessCollector.class);
-	
+
 	@Autowired
 	private TrustProfileService profileService;
+
+	@Autowired
+	IdentityServiceClient identityServiceClient;
 	
-	
+	@Autowired
+	CatalogServiceClient catalogServiceClient;
+
 	@Transactional
-	public void obtainNewValueCompanyDetails(String companyId) {
-		String newValue = "0.25";
-		profileService.updateTrustAttributeValue(companyId, Trust.ProfileCompletnessDetails.getLocalName(), newValue);
+	public void obtainNewValueCompanyProfile(String companyId, String attributeTypeName) {
+		String newValue = getNewValue(companyId, attributeTypeName);
+		profileService.updateTrustAttributeValue(companyId, attributeTypeName, newValue);
 		recalculateScoreAndSaveIt(companyId);
 		recalculateTrustScoreAndSaveIt(companyId);
 	}
 
-	@Transactional
-	public void obtainNewValueCompanyTrade(String companyId) {
+	private String getNewValue(String companyId, String attributeTypeName) {
+
 		String newValue = "0.25";
-		profileService.updateTrustAttributeValue(companyId, Trust.ProfileCompletnessTrade.getLocalName(), newValue);
-		recalculateScoreAndSaveIt(companyId);
-		recalculateTrustScoreAndSaveIt(companyId);
+
+//		if (attributeTypeName.equals(Trust.ProfileCompletnessCertificates.getLocalName())) {
+//			identityServiceClient.getParty(..
+//		}
+//		if (attributeTypeName.equals(Trust.ProfileCompletnessDetails.getLocalName())) {
+//
+//		}
+//		if (attributeTypeName.equals(Trust.ProfileCompletnessDescription.getLocalName())) {
+//
+//		}
+//		if (attributeTypeName.equals(Trust.ProfileCompletnessTrade.getLocalName())) {
+//
+//		}
+
+		return newValue;
 	}
 
-	@Transactional
-	public void obtainNewValueCompanyCertificates(String companyId) {
-		String newValue = "0.25";
-		profileService.updateTrustAttributeValue(companyId, Trust.ProfileCompletnessCertificates.getLocalName(), newValue);
-		recalculateScoreAndSaveIt(companyId);
-		recalculateTrustScoreAndSaveIt(companyId);
-	}
-
-	@Transactional
-	public void obtainNewValueCompanyDescription(String companyId) {
-		String newValue = "0.25";
-		profileService.updateTrustAttributeValue(companyId, Trust.ProfileCompletnessDescription.getLocalName(), newValue);
-		recalculateScoreAndSaveIt(companyId);
-		recalculateTrustScoreAndSaveIt(companyId);
-	}
-	
-	
-	public void recalculateScoreAndSaveIt(String companyId){
+	public void recalculateScoreAndSaveIt(String companyId) {
 		TrustProfile profile = profileService.findByAgentAltId(companyId);
-		TrustAttribute attr1= profile.findAttribute(Trust.ProfileCompletnessTrade.getLocalName());
-		TrustAttribute attr2= profile.findAttribute(Trust.ProfileCompletnessCertificates.getLocalName());
-		TrustAttribute attr3= profile.findAttribute(Trust.ProfileCompletnessDescription.getLocalName());
-		TrustAttribute attr4= profile.findAttribute(Trust.ProfileCompletnessDetails.getLocalName());
-	
-		BigDecimal score = (attr1!=null)? new BigDecimal(attr1.getValue()):BigDecimal.ZERO;
-		score = score.add((attr2!=null)? new BigDecimal(attr2.getValue()):BigDecimal.ZERO);
-		score = score.add((attr3!=null)? new BigDecimal(attr3.getValue()):BigDecimal.ZERO);
-		score = score.add((attr4!=null)? new BigDecimal(attr4.getValue()):BigDecimal.ZERO);
-		score = (score.compareTo(BigDecimal.ZERO)==0)? BigDecimal.ZERO:score.divide(new BigDecimal(4L));
-		
-		profileService.updateTrustAttributeValue(companyId, Trust.OverallProfileCompletness.getLocalName(), score.toString());
+		TrustAttribute attr1 = profile.findAttribute(Trust.ProfileCompletnessTrade.getLocalName());
+		TrustAttribute attr2 = profile.findAttribute(Trust.ProfileCompletnessCertificates.getLocalName());
+		TrustAttribute attr3 = profile.findAttribute(Trust.ProfileCompletnessDescription.getLocalName());
+		TrustAttribute attr4 = profile.findAttribute(Trust.ProfileCompletnessDetails.getLocalName());
+
+		BigDecimal score = (attr1 != null) ? new BigDecimal(attr1.getValue()) : BigDecimal.ZERO;
+		score = score.add((attr2 != null) ? new BigDecimal(attr2.getValue()) : BigDecimal.ZERO);
+		score = score.add((attr3 != null) ? new BigDecimal(attr3.getValue()) : BigDecimal.ZERO);
+		score = score.add((attr4 != null) ? new BigDecimal(attr4.getValue()) : BigDecimal.ZERO);
+		score = (score.compareTo(BigDecimal.ZERO) == 0) ? BigDecimal.ZERO : score.divide(new BigDecimal(4L));
+
+		profileService.updateTrustAttributeValue(companyId, Trust.OverallProfileCompletness.getLocalName(),
+				score.toString());
 	}
-	
-	public void recalculateTrustScoreAndSaveIt(String companyId){
+
+	public void recalculateTrustScoreAndSaveIt(String companyId) {
+		
 		log.info("trust score updated");
 		
+		
+		//notify catalog service
+		PartyType party = new PartyType();
+		List<QualityIndicatorType> qualityIndicatorTypes = Lists.newArrayList();
+		QualityIndicatorType score = new QualityIndicatorType();
+		score.setQualityParameter("TrustScore");
+		QuantityType quantity = new QuantityType();
+		quantity.setValue(new BigDecimal("1"));
+		score.setQuantity(quantity);
+		party.setQualityIndicator(qualityIndicatorTypes);
+		
+//		catalogServiceClient.postTrustScoreUpdate(companyId, party);
+		
+
 	}
 
 }
--- src/main/java/nimble/trust/engine/service/ChangeEventHandlerService.java
+++ src/main/java/nimble/trust/engine/service/ChangeEventHandlerService.java
@@ -6,40 +6,42 @@ import org.springframework.stereotype.Service;
 import com.google.common.eventbus.AsyncEventBus;
 
 import nimble.trust.engine.collector.ProfileCompletnessCollector;
+import nimble.trust.engine.model.vocabulary.Trust;
 import nimble.trust.web.dto.ChangeEvent;
-
 
 @Service
 public class ChangeEventHandlerService {
-	
-	
+
 	@Autowired
 	private AsyncEventBus asyncEventBus;
-	
+
 	@Autowired
 	private ProfileCompletnessCollector profileCompletnessCollector;
 
 	public void postChangeEvent(ChangeEvent changeEvent) throws Exception {
 		asyncEventBus.post(changeEvent);
 	}
-	
-	
-	public void handleChangeEvent(ChangeEvent changeEvent) throws Exception{
-		
+
+	public void handleChangeEvent(ChangeEvent changeEvent) throws Exception {
+
 		String type = changeEvent.getChangeType();
-		
-		if (type.equalsIgnoreCase("company_details")){
-			profileCompletnessCollector.obtainNewValueCompanyDetails(changeEvent.getCompanyIdentifier());
+
+		if (type.equalsIgnoreCase("company_details")) {
+			profileCompletnessCollector.obtainNewValueCompanyProfile(changeEvent.getCompanyIdentifier(),
+					Trust.ProfileCompletnessDetails.getLocalName());
 		}
-		if (type.equalsIgnoreCase("company_description")){
-			profileCompletnessCollector.obtainNewValueCompanyDescription(changeEvent.getCompanyIdentifier());
+		if (type.equalsIgnoreCase("company_description")) {
+			profileCompletnessCollector.obtainNewValueCompanyProfile(changeEvent.getCompanyIdentifier(),
+					Trust.ProfileCompletnessDescription.getLocalName());
 		}
-		if (type.equalsIgnoreCase("company_certificates")){
-			profileCompletnessCollector.obtainNewValueCompanyCertificates(changeEvent.getCompanyIdentifier());
+		if (type.equalsIgnoreCase("company_certificates")) {
+			profileCompletnessCollector.obtainNewValueCompanyProfile(changeEvent.getCompanyIdentifier(),
+					Trust.ProfileCompletnessCertificates.getLocalName());
 		}
-		if (type.equalsIgnoreCase("company_trade")){
-			profileCompletnessCollector.obtainNewValueCompanyTrade(changeEvent.getCompanyIdentifier());
+		if (type.equalsIgnoreCase("company_trade")) {
+			profileCompletnessCollector.obtainNewValueCompanyProfile(changeEvent.getCompanyIdentifier(),
+					Trust.ProfileCompletnessTrade.getLocalName());
 		}
 	}
-	
+
 }
