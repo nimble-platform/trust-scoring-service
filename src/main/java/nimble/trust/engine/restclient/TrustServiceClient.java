bc89d45005397199fa49c2d17db3f464f0bc3dfd
vujasm <m.vujasinovic@innova-eu.net>
2018-10-05 13:34:28 +0200

    revert 'test without TokenAuthenticationFilter'

--- src/main/java/nimble/trust/engine/restclient/TrustServiceClient.java
+++ src/main/java/nimble/trust/engine/restclient/TrustServiceClient.java
@@ -1,10 +1,11 @@
 package nimble.trust.engine.restclient;
 
 import feign.Response;
-
+import io.swagger.annotations.ApiParam;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import eu.nimble.service.model.ubl.commonaggregatecomponents.PartyType;
@@ -17,5 +18,9 @@ public interface TrustServiceClient {
 	
     @RequestMapping(method = RequestMethod.GET, value = "/version", produces = "application/json")
     Response getVersion();
+    
+    
+    @RequestMapping(value = "/party/{partyId}/trust",produces = MediaType.APPLICATION_JSON_VALUE,method = RequestMethod.GET)
+    Response getPartyTrustData( @RequestHeader(value = "Authorization") String bearerToken, @PathVariable("partyId") String partyId);
     
 }
