e318288d315a756848e42bc60518b311b54e3c9b
Johannes Innerbichler <j.innerbichler@gmail.com>
2018-11-14 17:04:20 +0100

    Created hotfix for proper Kafka configuration.

--- src/main/java/nimble/trust/config/KafkaConfig.java
+++ src/main/java/nimble/trust/config/KafkaConfig.java
@@ -41,9 +41,11 @@ public class KafkaConfig {
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
-        props.put("sasl.mechanism", "PLAIN");
-        props.put("sasl.jaas.config", jaasConfig);
+        if (jaasConfig != null && jaasConfig.isEmpty() == false) {
+            props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+            props.put("sasl.mechanism", "PLAIN");
+            props.put("sasl.jaas.config", jaasConfig);
+        }
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return props;
