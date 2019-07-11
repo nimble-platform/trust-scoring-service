bdc6146562afd2ece74703c755c1cf99b68be8bd
Akayeshmantha <akayeshmantha@apache.org>
2019-07-11 16:19:39 +0200

    Adding feign fallback client.

---
+++ src/main/java/nimble/trust/engine/restclient/IndexingClientFallback.java
@@ -0,0 +1,28 @@
+package nimble.trust.engine.restclient;
+
+import eu.nimble.service.model.solr.party.PartyType;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Ayeshmantha Perera
+ */
+@Component
+public class IndexingClientFallback implements IndexingClient {
+
+    @Override
+    public Boolean setParty(PartyType party) {
+        return false;
+    }
+
+    @Override
+    public PartyType getParty(String uri) {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteParty(PartyType party) {
+        return false;
+    }
+}
