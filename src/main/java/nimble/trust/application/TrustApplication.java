719d1143138a61a597dce92287677f3f818d26cd
Akayeshmantha <akayeshmantha@apache.org>
2019-08-08 17:03:55 +0200

    Hystrix added.

--- src/main/java/nimble/trust/application/TrustApplication.java
+++ src/main/java/nimble/trust/application/TrustApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -27,8 +28,8 @@ import nimble.trust.config.AuditorAwarer;
 @Configuration
 @EnableCircuitBreaker
 @EnableAutoConfiguration
+@EnableHystrix
 @EnableEurekaClient
-@EnableDiscoveryClient
 @EnableFeignClients(basePackages="nimble.trust.engine.restclient")
 @RestController
 @EnableJpaAuditing(auditorAwareRef="auditorProvider")
