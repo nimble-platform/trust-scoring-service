91c94f496809e76d963b40b9d96acfdec8e3b7ce
Akayeshmantha <akayeshmantha@apache.org>
2019-08-08 15:20:53 +0200

    trust quick fix.

--- src/main/java/nimble/trust/application/TrustApplication.java
+++ src/main/java/nimble/trust/application/TrustApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -27,6 +28,7 @@ import nimble.trust.config.AuditorAwarer;
 @EnableCircuitBreaker
 @EnableAutoConfiguration
 @EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients(basePackages="nimble.trust.engine.restclient")
 @RestController
 @EnableJpaAuditing(auditorAwareRef="auditorProvider")
