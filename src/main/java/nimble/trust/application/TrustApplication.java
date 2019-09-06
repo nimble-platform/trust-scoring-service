0335c6e632f1cc6b7434f388258c988dc0b8f4e8
Akayeshmantha <akayeshmantha@apache.org>
2019-09-06 12:40:45 +0200

    quick fix.

--- src/main/java/nimble/trust/application/TrustApplication.java
+++ src/main/java/nimble/trust/application/TrustApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
