a90b84f55d7d7fb32b260d5c5d7744cea9339e64
Akayeshmantha <akayeshmantha@apache.org>
2019-09-06 12:17:03 +0200

    fix discovery.

--- src/main/java/nimble/trust/application/TrustApplication.java
+++ src/main/java/nimble/trust/application/TrustApplication.java
@@ -11,7 +11,6 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -28,7 +27,6 @@ import nimble.trust.config.AuditorAwarer;
 @Configuration
 @EnableCircuitBreaker
 @EnableAutoConfiguration
-@EnableHystrix
 @EnableEurekaClient
 @EnableFeignClients(basePackages="nimble.trust.engine.restclient")
 @RestController
