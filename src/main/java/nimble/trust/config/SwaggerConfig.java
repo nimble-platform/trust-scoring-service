152897fa91ce312d22d8c86b9f4bd7ed984ae4e6
Akayeshmantha <akayeshmantha@apache.org>
2019-09-06 14:53:58 +0200

    swagger fix.

--- src/main/java/nimble/trust/config/SwaggerConfig.java
+++ src/main/java/nimble/trust/config/SwaggerConfig.java
@@ -1,8 +1,10 @@
 package nimble.trust.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -12,12 +14,23 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-   
+
+
+	@Value("${${nimble.trust.url:}}")
+	private String platformHost;
+
 	@Bean
     public Docket api() {
+
+		platformHost = platformHost.replace("https://", "");
+		platformHost = platformHost.replace("http://","");
+
         return new Docket(DocumentationType.SWAGGER_2)
+		        .host(platformHost)
                 .select().apis(RequestHandlerSelectors.basePackage("nimble.trust"))
-                .build().apiInfo(apiInfo());
+		        .paths(PathSelectors.any())
+		        .build()
+		        .apiInfo(apiInfo())    ;
     }
 	
 	private ApiInfo apiInfo() {
