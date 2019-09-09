b45a5f3efb7a483c548434d9cfe2e568ba4dc1eb
Akayeshmantha <akayeshmantha@apache.org>
2019-09-09 17:21:06 +0200

    revert back trust changes.

--- src/main/java/nimble/trust/application/TrustApplication.java
+++ src/main/java/nimble/trust/application/TrustApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -26,7 +25,8 @@ import nimble.trust.config.AuditorAwarer;
 
 @Configuration
 @EnableCircuitBreaker
-@EnableDiscoveryClient
+@EnableAutoConfiguration
+@EnableEurekaClient
 @EnableFeignClients(basePackages="nimble.trust.engine.restclient")
 @RestController
 @EnableJpaAuditing(auditorAwareRef="auditorProvider")
