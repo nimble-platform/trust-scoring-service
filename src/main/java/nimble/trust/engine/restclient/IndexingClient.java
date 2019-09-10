c8affb1c8d06c107d9ff440e09a1eff48d17dc6c
Akayeshmantha <akayeshmantha@apache.org>
2019-09-10 11:15:29 +0200

    updated indexing client.

--- src/main/java/nimble/trust/engine/restclient/IndexingClient.java
+++ src/main/java/nimble/trust/engine/restclient/IndexingClient.java
@@ -1,33 +1,26 @@
-//package nimble.trust.engine.restclient;
-//
-//import eu.nimble.service.model.solr.party.PartyType;
-//import feign.Headers;
-//import org.springframework.cloud.netflix.feign.FeignClient;
-//import org.springframework.web.bind.annotation.RequestHeader;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-///**
-// * REST Client to index entities via the Indexing Service.
-// *
-// * @author Ayeshmantha Perera
-// */
-//@FeignClient(name = "indexing-service", url = "${nimble.indexing.url}", fallback = IndexingClientFallback.class)
-//public interface IndexingClient {
-//    @RequestMapping(method = RequestMethod.PUT, value = "/party", consumes = "application/json")
-//    @Headers("Content-Type: application/json")
-//    Boolean setParty(@RequestBody PartyType party,
-//            @RequestHeader(value = "Authorization", required = true) String bearerToken);
-//
-//    @RequestMapping(method = RequestMethod.GET, value = "/party")
-//    PartyType getParty(@RequestParam(value = "uri") String uri,
-//            @RequestHeader(value = "Authorization", required = true) String bearerToken);
-//
-//    @RequestMapping(method = RequestMethod.DELETE, value = "/party")
-//    Boolean deleteParty(@RequestBody PartyType party,
-//            @RequestHeader(value = "Authorization", required = true) String bearerToken);
-//
-//
-//}
+package nimble.trust.engine.restclient;
+
+import eu.nimble.service.model.ubl.commonaggregatecomponents.PartyType;
+import feign.Headers;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * REST Client to index entities via the Indexing Service.
+ *
+ * @author Ayeshmantha Perera
+ */
+@FeignClient(name = "indexing-service", url = "${nimble.indexing.url}", fallback = IndexingClientFallback.class)
+public interface IndexingClient {
+
+	@RequestMapping(method = RequestMethod.POST, value = "/party/trust")
+	Boolean partyTrustUpdate(
+			@RequestParam(value = "partyId") String partyId,
+			@RequestBody PartyType partyType,
+			@RequestHeader(value = "Authorization", required = true) String bearerToken);
+
+}
--- src/main/java/nimble/trust/engine/restclient/IndexingClientFallback.java
+++ src/main/java/nimble/trust/engine/restclient/IndexingClientFallback.java
@@ -1,28 +1,20 @@
-//package nimble.trust.engine.restclient;
-//
-//import eu.nimble.service.model.solr.party.PartyType;
-//
-//import org.springframework.stereotype.Component;
-//
-///**
-// *
-// * @author Ayeshmantha Perera
-// */
-//@Component
-//public class IndexingClientFallback implements IndexingClient {
-//
-//    @Override
-//    public Boolean setParty(PartyType party,String bearerToken) {
-//        return false;
-//    }
-//
-//    @Override
-//    public PartyType getParty(String uri,String bearerToken) {
-//        return null;
-//    }
-//
-//    @Override
-//    public Boolean deleteParty(PartyType party,String bearerToken) {
-//        return false;
-//    }
-//}
+package nimble.trust.engine.restclient;
+
+import eu.nimble.service.model.ubl.commonaggregatecomponents.PartyType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Ayeshmantha Perera
+ */
+@Component
+public class IndexingClientFallback implements IndexingClient {
+    @Override
+    public Boolean partyTrustUpdate(String partyId,PartyType partyType,String bearerToken) {
+        return false;
+    }
+}
