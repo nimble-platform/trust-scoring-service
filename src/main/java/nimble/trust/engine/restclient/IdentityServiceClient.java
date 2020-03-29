196bd96041eedd020fa55c5c11c569921efe8d4a
Akayeshmantha <akayeshmantha@apache.org>
2020-03-30 01:41:20 +0200

    fix identity calls.

--- src/main/java/nimble/trust/engine/restclient/IdentityServiceClient.java
+++ src/main/java/nimble/trust/engine/restclient/IdentityServiceClient.java
@@ -10,21 +10,21 @@ import org.springframework.web.bind.annotation.*;
 /**
  * Feign client for identity microservice.
  */
-@FeignClient(name = "identity-service", url = "${nimble.identity.url:}")
+@FeignClient(name = "identity-service", url = "${nimble.identity.url:}",fallback = IdentityServiceClientFallBack.class)
 public interface IdentityServiceClient {
     
 	@RequestMapping(method = RequestMethod.GET, value = "/party/{partyId}", produces = "application/json")
-    Response getParty(@RequestHeader("Authorization") String bearerToken, @PathVariable("partyId") String partyId);
-	
-//	@RequestMapping(method = RequestMethod.GET, value = "/party/completeness/{partyId}", produces = "application/json")
-//    Response getPartyTrust(@RequestHeader("Authorization") String bearerToken, @PathVariable("partyId") String partyId);
-	
-    @RequestMapping(value = "/company-settings/{partyId}/completeness", produces = {"application/json"}, method = RequestMethod.GET)
-	Response getProfileCompleteness(@RequestHeader("Authorization") String bearerToken, @PathVariable("partyId") String partyId);
+	Response getParty(@RequestHeader("Authorization") String bearerToken, @PathVariable("partyId") String partyId);
 
-    @RequestMapping(method = RequestMethod.GET, value = "/parties/{partyIds}", produces = "application/json")
-    Response getParties(@RequestHeader("Authorization") String bearerToken, @PathVariable("partyIds") String partyIds);
+	//	@RequestMapping(method = RequestMethod.GET, value = "/party/completeness/{partyId}", produces = "application/json")
+	//    Response getPartyTrust(@RequestHeader("Authorization") String bearerToken, @PathVariable("partyId") String partyId);
 
-    @RequestMapping(method = RequestMethod.GET, value = "/party/all", produces = "application/json")
-    Response getAllPartyIds(@RequestHeader("Authorization") String bearerToken, @RequestParam(value = "exclude", required = false) List<String> exclude);
+	@RequestMapping(value = "/company-settings/{partyId}/completeness", produces = {"application/json"}, method = RequestMethod.GET)
+	Response getProfileCompleteness(@PathVariable("partyId") String partyId);
+
+	@RequestMapping(method = RequestMethod.GET, value = "/parties/{partyIds}", produces = "application/json")
+	Response getParties(@RequestHeader("Authorization") String bearerToken, @PathVariable("partyIds") String partyIds);
+
+	@RequestMapping(method = RequestMethod.GET, value = "/party/all", produces = "application/json")
+	Response getAllPartyIds();
 }
